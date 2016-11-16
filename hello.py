be1dc04265395db365a0f11173fd41179ce8cbf5
Albiglittle <molchanov.art13@gmail.com>
2016-11-16 21:28:51 +0300

    one another attempt

--- hello.py
+++ hello.py
@@ -1,17 +1,11 @@
-from cgi import parse_qs
-
-
-def application(environ, start_response):
-    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
-    body = []
-    for key, values in query.items():
-        for item in values:
-            body.append(key + "=" + item + "\r\n")
-
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = '\n'.join(environ['QUERY_STRING'].split('&'))
+    # data = 'Hello, World!\n'
     status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
+    response_headers = [
+        ('Content-type','text/plain'),
+        ('Content-Length', str(len(data)))
     ]
-
-    start_response(status, headers)
-    return body
+    start_response(status, response_headers)
+    return iter([data])
