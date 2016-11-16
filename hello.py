a874a23527de24e6424d6888c89113e345f12972
Albiglittle <molchanov.art13@gmail.com>
2016-11-16 21:17:42 +0300

    redaction

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-from cgi import parse
+from cgi import parse_qs
 
 
 def application(environ, start_response):
-    query = parse(environ['QUERY_STRING'], keep_blank_values=True)
+    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
     body = []
     for key, values in query.items():
         for item in values:
